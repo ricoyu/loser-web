39fbccdfc7bb8ded1892e7598db7894e5b0da926
Star Lord <ricoyu520@gmail.com>
2019-11-13 18:52:49 +0800

    包结构整理

---
+++ src/main/java/com/loserico/web/enums/StatusCode.java
@@ -0,0 +1,69 @@
+package com.loserico.web.enums;
+
+/**
+ * 异常代码，code是相关代码、msgTemplate是国际化消息模版、defaultMsg是模版对应的国际化消息不存在情况下的默认消息 
+ * <p>
+ * Copyright: Copyright (c) 2019-10-14 16:23
+ * <p>
+ * Company: Sexy Uncle Inc.
+ * <p>
+ 
+ * @author Rico Yu  ricoyu520@gmail.com
+ * @version 1.0
+ */
+public enum StatusCode {
+
+	/* 40xx 数据验证相关code */
+	BAD_REQUEST(4000, "code.badrequest", "Bad Request"),
+	DUPLICATE_SUBMISSION(4022, "duplicate.submit", "请勿重复提交"),
+
+	/* 41xx 数据交互相关code */
+	ENTITY_NOT_FOUND(4101, "code.entity.not.found", "请求的数据不存在"),
+	UNIQUE_VIOLATION(4103, "code.unique.violation", "违反唯一性约束"),
+	USER_NOT_EXISTS(4208, "user.not.exists", "该用户不存在"),
+	ACCOUNT_NOT_EXIST(4211, "account.not.exist", "您的账号不存在"),
+	NO_MOBILE(4214, "not.empty.mobile", "请提供mobile"),
+	FIRST_LOGIN(4215, "user.first.login", "首次登录请修改密码"),
+	ACCOUNT_EXISTS(4216, "account.exists", "账号已存在"),
+	API_FILEUPLOAD_FAIL(4301, "file.upload.failed", "上传图片失败"),
+
+	/* 50xx 未知异常 */
+	INTERNAL_SERVER_ERROR(5000, "code.internal.server.error", "Internal Server Error");
+
+	private int code;
+	private String defaultMsg;
+	private String msgTemplate;
+
+	private StatusCode(int code, String msgTemplate, String defaultMsg) {
+		this.code = code;
+		this.msgTemplate = msgTemplate;
+		this.defaultMsg = defaultMsg;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+
+	public String getMsgTemplate() {
+		return msgTemplate;
+	}
+
+	public void setMsgTemplate(String msgTemplate) {
+		this.msgTemplate = msgTemplate;
+	}
+
+
+	public String getDefaultMsg() {
+		return defaultMsg;
+	}
+
+	public void setDefaultMsg(String defaultMsg) {
+		this.defaultMsg = defaultMsg;
+	}
+
+}
---
+++ src/main/java/com/loserico/web/utils/DateUtils.java
@@ -0,0 +1,129 @@
+package com.loserico.web.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.ref.SoftReference;
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TimeZone;
+
+import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+/**
+ * 日期格式化/转换相关帮助类
+ * <p>
+ * Copyright: Copyright (c) 2019/10/14 14:22
+ * <p>
+ * Company: Sexy Uncle Inc.
+ * <p>
+ *
+ * @author Rico Yu  ricoyu520@gmail.com
+ * @version 1.0
+ */
+@Slf4j
+public class DateUtils {
+
+	public static Map<TimeZone, Locale> timeZoneLocaleMap = new HashMap<>();
+
+	public static TimeZone CHINA = TimeZone.getTimeZone("Asia/Shanghai");
+	
+	public static final String FMT_ISO_DATE = "yyyy-MM-dd";
+	
+	public static final String FMT_ISO_DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+
+	/**
+	 * 根据指定的format解析日期字符串, 时区为"Asia/Shanghai", Locale为CHINA
+	 *
+	 * @param source
+	 * @param format
+	 * @return Date
+	 */
+	public static Date parse(String source, String format) {
+		if (isBlank(source)) {
+			return null;
+		}
+		Objects.requireNonNull(format);
+		SimpleDateFormat simpleDateFormat = SimpleDateFormatHolder.formatFor(format);
+		try {
+			return simpleDateFormat.parse(source);
+		} catch (ParseException e) {
+			log.error(MessageFormat.format("Parse date string:[{0}]", source));
+		}
+		return null;
+	}
+	
+
+	final static class SimpleDateFormatHolder {
+
+		private static final ThreadLocal<SoftReference<Map<String, SimpleDateFormat>>> THREADLOCAL_FORMATS = new ThreadLocal<SoftReference<Map<String, SimpleDateFormat>>>();
+
+		/**
+		 * 获取SimpleDateFormat对象，timezone默认为Asia/Shanghai，locale为SIMPLIFIED_CHINESE
+		 *
+		 * @param pattern
+		 * @return
+		 */
+		public static SimpleDateFormat formatFor(final String pattern) {
+			requireNonNull(pattern);
+			final SoftReference<Map<String, SimpleDateFormat>> ref = THREADLOCAL_FORMATS.get();
+			Map<String, SimpleDateFormat> formats = ref == null ? null : ref.get();
+			if (formats == null) {
+				formats = new HashMap<String, SimpleDateFormat>();
+				THREADLOCAL_FORMATS.set(new SoftReference<Map<String, SimpleDateFormat>>(formats));
+			}
+
+			SimpleDateFormat format = formats.get(pattern);
+			if (format == null) {
+				format = new SimpleDateFormat(pattern, Locale.CHINA);
+				format.setTimeZone(CHINA);
+				formats.put(pattern, format);
+			}
+
+			return format;
+		}
+
+		/**
+		 * 根据format和timezone获取SimpleDateFormat对象，根据时区决定locale是什么
+		 *
+		 * @param pattern
+		 * @param timezone
+		 * @return
+		 */
+		public static SimpleDateFormat formatFor(final String pattern, TimeZone timezone) {
+			requireNonNull(pattern);
+			final SoftReference<Map<String, SimpleDateFormat>> ref = THREADLOCAL_FORMATS.get();
+			Map<String, SimpleDateFormat> formats = ref == null ? null : ref.get();
+			if (formats == null) {
+				formats = new HashMap<String, SimpleDateFormat>();
+				THREADLOCAL_FORMATS.set(new SoftReference<Map<String, SimpleDateFormat>>(formats));
+			}
+
+			SimpleDateFormat format = formats.get(pattern + timezone.getID());
+			if (format == null) {
+				Locale locale = timeZoneLocaleMap.get(timezone.getID());
+				if (locale == null) {
+					format = new SimpleDateFormat(pattern);
+				} else {
+					format = new SimpleDateFormat(pattern, locale);
+				}
+				format.setTimeZone(timezone);
+				formats.put(pattern + timezone.getID(), format);
+			}
+
+			return format;
+		}
+
+		public static void clearThreadLocal() {
+			THREADLOCAL_FORMATS.remove();
+		}
+
+	}
+}
